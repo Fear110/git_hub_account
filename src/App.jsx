267cfd447fbc0360a0d9dae96ad54a59f58a4209
Sakar Pudasaini <barcaboysakar@gmil.com>
2023-05-12 20:14:08 +0545

    Finally doing smth

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,36 @@ import { useEffect } from 'react';
 // api = `https://api.github.com/users/fear110`;
 
 export default function App() {
+  const [name, setName] = useState('');
   const [data, setdata] = useState('');
-  const apiurl = `https://api.github.com/users/fear110`;
+  const apiurl = `https://api.github.com/users/${name}`;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const resopnse = await fetch(apiurl);
-      const data = await resopnse.json();
-      console.log(data);
-    };
-    fetchData();
-  }, []);
+  const fetchSearchResults = async () => {
+    const resopnse = await fetch(apiurl);
+    const data = await resopnse.json();
+    setdata(data);
+    console.log(data);
+  };
+
+  function setSearchResults(event) {
+    if (event.key === 'Enter') {
+      fetchSearchResults();
+    }
+  }
 
   return (
     <div>
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Enter Your Name"
+          value={name}
+          onChange={event => setName(event.target.value)}
+          onKeyPress={setSearchResults}
+        />
+      </div>
       <p> name is {data.name}</p>
+      <p>new name is {name} </p>
     </div>
   );
 }
